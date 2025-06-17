b49c327e129509a870762ab39cfb981216de27be
Fede Rudiero <federudiero@gmail.com>
2025-06-17 10:24:11 -0300

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import BlossomNavbar from './components/BlossomNavbar';
 import Hero from './components/Hero';
 import About from './components/About';
-
 import Gallery from './components/Gallery';
-
 import Footer from './components/Footer';
 import Products from './components/Products';
-import Cart from './components/Cart'; // Componente de carrito
-import { CartProvider } from './context/CartContext';
+import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import BenefitsBar from './components/BenefitsBar';
 import AdminPanelModal from './components/AdminPanelModal';
-
-
-
-
+import { CartProvider } from './context/CartContext';
 
 function App() {
+  const [showAdmin, setShowAdmin] = useState(false); // Estado para mostrar el modal
+
   return (
     <CartProvider>
       <Router>
@@ -30,19 +27,22 @@ function App() {
             element={
               <>
                 <Hero />
-              <BenefitsBar/>
+                <BenefitsBar />
                 <About />
                 <Gallery />
-                <BenefitsBar/>
+                <BenefitsBar />
                 <Products />
-            
               </>
             }
           />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
- <Footer onAdminClick={() => setShowAdmin(true)} />
+
+        {/* Footer con botón secreto para abrir modal */}
+        <Footer onAdminClick={() => setShowAdmin(true)} />
+
+        {/* Modal de administración */}
         <AdminPanelModal show={showAdmin} onHide={() => setShowAdmin(false)} />
       </Router>
     </CartProvider>
